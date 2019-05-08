187aef8408899a1ad84cfcbbc4ff46bf454122b0
LeeJinho <kartmon@naver.com>
2019-05-09 02:10:29 +0900

    first upload blog web

---
+++ blogproject/blogproject/urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.urls import path, include
+import blogapp.views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',blogapp.views.home,name="home"),
+    path('blog/<int:blog_id>',blogapp.views.detail,name='detail')
+]
